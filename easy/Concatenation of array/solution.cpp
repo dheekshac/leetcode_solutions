c9fcb5263914f47ba915306661fdf395ad0d3263
dheekshac <dheekshac07@gmail.com>
2026-03-24 23:31:26 +0530

    Create solution.cpp
    class Solution {
    public:
        vector<int> getConcatenation(vector<int>& nums) {
            vector<int> ans;
            int n=nums.size()-1;
            for(int i=0;i<nums.size();i++){
                ans.push_back(nums[i]);
            }
            for(int i=0;i<nums.size();i++){
                ans.push_back(nums[i]);
            }
            
           return ans;
        }
    };

---
+++ easy/Concatenation of array/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> ans;
+        int n=nums.size()-1;
+        for(int i=0;i<nums.size();i++){
+            ans.push_back(nums[i]);
+        }
+        for(int i=0;i<nums.size();i++){
+            ans.push_back(nums[i]);
+        }
+        
+       return ans;
+    }
+};
